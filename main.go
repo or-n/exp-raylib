ca5d2d40de637354c3a64a8368c5928e9a7c8e66
or-n <kulombowie@gmail.com>
2025-05-02 02:14:57 +0200

    map

--- main.go
+++ main.go
@@ -1,24 +1,29 @@
 package main
 
 import . "github.com/gen2brain/raylib-go/raylib"
-import "fmt"
+import (
+	"math/rand"
+	"time"
+)
 
 var WindowBg = NewColor(127, 31, 255, 255)
 
 func main() {
+    rand.Seed(time.Now().UnixNano())
     InitWindow(1920, 1920, "Hello")
     defer CloseWindow()
     ToggleFullscreen()
-    size := NewVector2(float32(GetMonitorWidth(0)), float32(GetMonitorHeight(0)))
-    fmt.Println(size)
+    // size := NewVector2(float32(GetMonitorWidth(0)), float32(GetMonitorHeight(0)))
     SetTargetFPS(600)
     PlayerInit()
     InputInit()
+    MapInit()
     for !WindowShouldClose() {
         PlayerUpdate()
         BeginDrawing()
         ClearBackground(WindowBg)
         DrawFPS(30, 30)
+        MapDraw()
         PlayerDraw()
         EndDrawing()
     }
---
+++ map.go
@@ -0,0 +1,39 @@
+package main
+
+import . "github.com/gen2brain/raylib-go/raylib"
+import "math/rand"
+
+const (
+	max_x = 100
+	max_y = 10
+)
+
+var (
+	data [max_y][max_x]int
+	texture Texture2D
+	texture_x int32
+	texture_y int32
+)
+
+func MapInit() {
+	for y := 0; y < max_y; y++ {
+		for x := 0; x < max_x; x++ {
+			data[y][x] = rand.Intn(2)
+		}
+	}
+	texture = LoadTexture("asset/dirt.png")
+	texture_x = 16
+	texture_y = 16
+}
+
+func MapDraw() {
+	for y := 0; y < max_y; y++ {
+		position_y := int32(y) * texture_y
+		for x := 0; x < max_x; x++ {
+			if data[y][x] == 1 {
+				position_x := int32(x) * texture_x
+				DrawTexture(texture, position_x, position_y, White)
+			}
+		}
+	}
+}
