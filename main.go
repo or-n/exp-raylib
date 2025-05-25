5bebb7dd9a0688e8feb4f33f3d9287dbaf365462
or-n <kulombowie@gmail.com>
2025-05-25 22:08:20 +0200

    bg, change splits by dragging

--- main.go
+++ main.go
@@ -9,102 +9,107 @@ var (
 	pl         = []rune("ąćęłńóśźżĄĆĘŁŃÓŚŹŻ")
 	symbols    = []rune(" .,!?:;_()/")
 	codepoints = append(append(en, pl...), symbols...)
-	MainFont   Font
 	fontFile   = "asset/FiraCode-Bold.ttf"
 )
 
 func main() {
-	screenWidth := int32(1920) / 2
-	screenHeight := int32(1080) / 2
-
-	InitWindow(screenWidth, screenHeight, "raylib-go")
+	screenWidth := int32(1920)
+	screenHeight := int32(1080)
+	InitWindow(screenWidth, screenHeight, "")
 	defer CloseWindow()
-
 	SetTargetFPS(60)
 	ToggleFullscreen()
-
-	MainFont = LoadFontEx(fontFile, 20, codepoints, int32(len(codepoints)))
-
-	margin := float32(20) / float32(1080)
+	MainFont := LoadFontEx(fontFile, 20, codepoints, int32(len(codepoints)))
+	Bg := LoadTexture("asset/bg.jpg")
 	w := float32(screenWidth)
 	h := float32(screenHeight)
-	m := h * margin
-
-	var (
-		panelLocalX       = m
-		panelLocalY       = m
-		panelLocalWidth   = w/3 - m - m/2
-		panelLocalHeight  = h/2 - m - m/2
-		panelServerX      = w/3 + m/2
-		panelServerY      = m
-		panelServerWidth  = w/3 - m
-		panelServerHeight = h/2 - m - m/2
-		panelCmdX         = w*2/3 + m/2
-		panelCmdY         = m
-		panelCmdWidth     = w/3 - m/2 - m
-		panelCmdHeight    = h/4 - m - m/2
-		panelInfoX        = w*2/3 + m/2
-		panelInfoY        = h/4 + m/2
-		panelInfoWidth    = w/3 - m/2 - m
-		panelInfoHeight   = h/4 - m
-		panelLogX         = m
-		panelLogY         = h/2 + m/2
-		panelLogWidth     = w - m*2
-		panelLogHeight    = h/2 - m/2 - m
-	)
-
+	m := h * 20 / 1080
+	r := Rectangle{}
+	splitX1 := w * 0.25
+	splitX2 := w * 0.5
+	splitY1 := h * 0.25
+	splitY2 := h * 0.5
+	p := func(line float32) Vector2 {
+		return NewVector2(r.X+m, r.Y+m+20*line)
+	}
+	changeSplit := func(split *bool, r Rectangle) {
+		if CheckCollisionPointRec(GetMousePosition(), r) {
+			if IsMouseButtonPressed(MouseButtonLeft) {
+				*split = true
+			}
+		}
+	}
+	opacity := uint8(196)
+	textColor := NewColor(255, 255, 255, 255)
+	changeSplitX1 := false
+	changeSplitX2 := false
+	changeSplitY1 := false
+	changeSplitY2 := false
 	for !WindowShouldClose() {
+		cursor := GetMousePosition()
+		if IsMouseButtonDown(MouseButtonLeft) {
+			if changeSplitX1 {
+				splitX1 = cursor.X
+			}
+			if changeSplitX2 {
+				splitX2 = cursor.X
+			}
+			if changeSplitY1 {
+				splitY1 = cursor.Y
+			}
+			if changeSplitY2 {
+				splitY2 = cursor.Y
+			}
+		} else {
+			changeSplitX1 = false
+			changeSplitX2 = false
+			changeSplitY1 = false
+			changeSplitY2 = false
+		}
+		changeSplit(&changeSplitX1, NewRectangle(splitX1-m/2, 0, m, splitY2+m/2))
+		changeSplit(&changeSplitX2, NewRectangle(splitX2-m/2, 0, m, splitY2+m/2))
+		changeSplit(&changeSplitY1, NewRectangle(splitX2-m/2, splitY1-m/2, w-(splitX2-m/2), m))
+		changeSplit(&changeSplitY2, NewRectangle(0, splitY2-m/2, w, m))
 		BeginDrawing()
-		ClearBackground(RayWhite)
-
-		DrawRectangle(
-			int32(panelLocalX), int32(panelLocalY),
-			int32(panelLocalWidth), int32(panelLocalHeight),
-			Color{R: 0, G: 120, B: 255, A: 255},
-		)
-		DrawTextEx(MainFont, "pliki lokalne",
-			Vector2{X: panelLocalX + m, Y: panelLocalY + m}, 20, 2, White)
-
-		DrawRectangle(
-			int32(panelServerX), int32(panelServerY),
-			int32(panelServerWidth), int32(panelServerHeight),
-			Color{R: 255, G: 0, B: 0, A: 255},
-		)
-		DrawTextEx(MainFont, "pliki serwerowe",
-			Vector2{X: panelServerX + m, Y: panelServerY + m}, 20, 2, White)
-
-		DrawRectangle(
-			int32(panelCmdX), int32(panelCmdY),
-			int32(panelCmdWidth), int32(panelCmdHeight),
-			Black,
-		)
-		DrawTextEx(MainFont, "lista podstawowych komend",
-			Vector2{X: panelCmdX + m, Y: panelCmdY + m}, 20, 2, White)
-
-		DrawRectangle(
-			int32(panelInfoX), int32(panelInfoY),
-			int32(panelInfoWidth), int32(panelInfoHeight),
-			Color{R: 128, G: 128, B: 128, A: 255},
-		)
-
-		DrawTextEx(MainFont, "podstawowe informacje:",
-			Vector2{X: panelInfoX + m, Y: panelInfoY + m}, 20, 2, White)
-		DrawTextEx(MainFont, "stan serwera: wł/wył",
-			Vector2{X: panelInfoX + m, Y: panelInfoY + m + 20}, 20, 2, White)
-		DrawTextEx(MainFont, "ilość osób",
-			Vector2{X: panelInfoX + m, Y: panelInfoY + m + 20*2}, 20, 2, White)
-		DrawTextEx(MainFont, "itp.",
-			Vector2{X: panelInfoX + m, Y: panelInfoY + m + 20*3}, 20, 2, White)
-
-		DrawRectangle(
-			int32(panelLogX), int32(panelLogY),
-			int32(panelLogWidth), int32(panelLogHeight),
-			Color{R: 255, G: 215, B: 0, A: 255},
-		)
-		DrawTextEx(MainFont, "logi",
-			Vector2{X: panelLogX + m, Y: panelLogY + m}, 20, 2, Black)
-
+		DrawTexture(Bg, 0, 0, White)
+		r.X = m
+		r.Y = m
+		r.Width = splitX1 - m*3/2
+		r.Height = splitY2 - m*3/2
+		DrawRectangleRec(r, NewColor(0, 120, 255, opacity))
+		DrawTextEx(MainFont, "pliki lokalne", p(0), 20, 2, textColor)
+		r.X = splitX1 + m/2
+		r.Y = m
+		r.Width = (splitX2 - splitX1) - m
+		r.Height = splitY2 - m*3/2
+		DrawRectangleRec(r, NewColor(255, 0, 0, opacity))
+		DrawTextEx(MainFont, "pliki serwerowe", p(0), 20, 2, textColor)
+		r.X = splitX2 + m/2
+		r.Y = m
+		r.Width = (w - splitX2) - m*3/2
+		r.Height = splitY1 - m*3/2
+		DrawRectangleRec(r, NewColor(0, 0, 0, opacity))
+		DrawTextEx(MainFont, "lista podstawowych komend", p(0), 20, 2, textColor)
+		if CheckCollisionPointRec(cursor, r) {
+			if IsMouseButtonPressed(MouseButtonLeft) {
+				changeSplitY1 = true
+			}
+		}
+		r.X = splitX2 + m/2
+		r.Y = splitY1 + m/2
+		r.Width = (w - splitX2) - m*3/2
+		r.Height = (splitY2 - splitY1) - m
+		DrawRectangleRec(r, NewColor(128, 128, 128, opacity))
+		DrawTextEx(MainFont, "podstawowe informacje:", p(0), 20, 2, textColor)
+		DrawTextEx(MainFont, "stan serwera: wł/wył", p(1), 20, 2, textColor)
+		DrawTextEx(MainFont, "ilość osób", p(2), 20, 2, textColor)
+		DrawTextEx(MainFont, "itp.", p(3), 20, 2, textColor)
+		r.X = m
+		r.Y = splitY2 + m/2
+		r.Width = w - m*2
+		r.Height = (h - splitY2) - m*3/2
+		DrawRectangleRec(r, NewColor(255, 215, 0, opacity))
+		DrawTextEx(MainFont, "logi", p(0), 20, 2, textColor)
 		EndDrawing()
 	}
-
 }
